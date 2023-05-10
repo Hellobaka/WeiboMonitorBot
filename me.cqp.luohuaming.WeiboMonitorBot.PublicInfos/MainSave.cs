68dfbd6c4a65a7d83b05223648ad1fb8aae71b6e
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-05-10 15:10:02 +0800

    添加项目文件。

---
+++ me.cqp.luohuaming.WeiboMonitorBot.PublicInfos/MainSave.cs
@@ -0,0 +1,33 @@
+using me.cqp.luohuaming.WeiboMonitorBot.Sdk.Cqp;
+using me.cqp.luohuaming.WeiboMonitorBot.Tool.IniConfig;
+using System.Collections.Generic;
+using System.IO;
+
+namespace me.cqp.luohuaming.WeiboMonitorBot.PublicInfos
+{
+    public static class MainSave
+    {
+        /// <summary>
+        /// 保存各种事件的数组
+        /// </summary>
+        public static List<IOrderModel> Instances { get; set; } = new List<IOrderModel>();
+        public static CQLog CQLog { get; set; }
+        public static CQApi CQApi { get; set; }
+        public static string AppDirectory { get; set; }
+        public static string ImageDirectory { get; set; }
+
+        static IniConfig configMain;
+        public static IniConfig ConfigMain
+        {
+            get
+            {
+                if (configMain != null)
+                    return configMain;
+                configMain = new IniConfig(Path.Combine(AppDirectory, "Config.ini"), System.Text.Encoding.UTF8);
+                configMain.Load();
+                return configMain;
+            }
+            set { configMain = value; }
+        }
+    }
+}
